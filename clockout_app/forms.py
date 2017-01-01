772b3819c430634cffeffc008e9ea7efdfc477f8
Ladybiss <github@ladybiss.it>
2017-01-01 21:17:29 +0100

    initial form created

---
+++ clockout_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class DataForm(forms.Form):
+    in_morning = forms.TimeField(label='Morning clock in',)
+    out_lunch = forms.TimeField(label='Lunch break start',)
+    in_lunch = forms.TimeField(label='Lunch break end',)
+    total_hours = forms.ChoiceField(label='Required working hours', initial=8, widget=forms.Select, choices=[(str(x), str(x)) for x in range(1, 25)])
+    min_lunch_length = forms.IntegerField(label='Mandatory lunch break duration', min_value=1, max_value=180, initial=30)
